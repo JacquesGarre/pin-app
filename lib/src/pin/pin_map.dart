c02a2dfa4f826b7bba8dd9ce0510f2b65d94108b
Jaks <jcqs.gr@gmail.com>
2024-07-21 14:58:33 +0200

    stream location subscription

--- lib/src/pin/pin_map.dart
+++ lib/src/pin/pin_map.dart
@@ -1,12 +1,13 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:pinz/src/location/location_controller.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_compass/flutter_compass.dart';
-
 
 class PinMap extends StatefulWidget {
   final LatLng? initialPosition;
@@ -29,6 +30,7 @@ class PinMapState extends State<PinMap> {
   final MapController _mapController = MapController();
   bool _isLoading = true;
   double _heading = 0;
+  StreamSubscription<Position>? _positionStreamSubscription;
 
   @override
   void initState() {
@@ -36,6 +38,13 @@ class PinMapState extends State<PinMap> {
     _markerPosition = widget.initialPosition;
     _setCurrentLocation();
     _initCompass();
+    _listenForLocationUpdates();
+  }
+
+  @override
+  void dispose() {
+    _positionStreamSubscription?.cancel();
+    super.dispose();
   }
 
   Future<void> _setCurrentLocation() async {
@@ -53,6 +62,15 @@ class PinMapState extends State<PinMap> {
         _isLoading = false;
       });
     }
+  }
+
+  void _listenForLocationUpdates() {
+    _positionStreamSubscription =
+        Geolocator.getPositionStream().listen((Position position) {
+      setState(() {
+        _currentLocation = LatLng(position.latitude, position.longitude);
+      });
+    });
   }
 
   void _initCompass() {
@@ -75,6 +93,11 @@ class PinMapState extends State<PinMap> {
     return (bearing * 180.0 / pi + 360.0) % 360.0;
   }
 
+  double _calculateDistance(LatLng from, LatLng to) {
+    const Distance distance = Distance();
+    return distance.as(LengthUnit.Meter, from, to);
+  }
+
   LatLng _initialCenter() {
     LatLng center = const LatLng(45.521563, -122.677433);
     if (widget.readOnly) {
@@ -90,59 +113,80 @@ class PinMapState extends State<PinMap> {
     if (_isLoading) {
       return const Center(child: CircularProgressIndicator());
     }
+
     final double bearing = _markerPosition != null && _currentLocation != null
         ? _calculateBearing(_currentLocation!, _markerPosition!)
         : 0.0;
-    return FlutterMap(
-      mapController: _mapController,
-      options: MapOptions(
-        initialCenter: _initialCenter(),
-        initialZoom: 13.0,
-        initialRotation: -_heading,
-        onTap: widget.readOnly
-            ? null
-            : (tapPosition, point) {
-                setState(() {
-                  _markerPosition = point;
-                  widget.onMarkerChanged(point);
-                });
-              },
-      ),
+    final double distance = _markerPosition != null && _currentLocation != null
+        ? _calculateDistance(_currentLocation!, _markerPosition!)
+        : 0.0;
+
+    return Column(
       children: [
-        TileLayer(
-          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          subdomains: const ['a', 'b', 'c'],
-          userAgentPackageName: 'com.example.app',
-        ),
-        MarkerLayer(
-          markers: [
-            if (_currentLocation != null && widget.readOnly)
-              Marker(
-                  point: _currentLocation!,
-                  width: 40.0,
-                  height: 40.0,
-                  child: Transform.rotate(
-                    angle: bearing * pi / 180,
-                    child: const Icon(
-                      Icons.swipe_up_alt,
-                      size: 50.0,
-                      color: Colors.blue,
+        Expanded(
+          child: FlutterMap(
+            mapController: _mapController,
+            options: MapOptions(
+              initialCenter: _initialCenter(),
+              initialZoom: 17.0,
+              initialRotation: -_heading,
+              onTap: widget.readOnly
+                  ? null
+                  : (tapPosition, point) {
+                      setState(() {
+                        _markerPosition = point;
+                        widget.onMarkerChanged(point);
+                      });
+                    },
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: const ['a', 'b', 'c'],
+                userAgentPackageName: 'com.example.app',
+              ),
+              MarkerLayer(
+                markers: [
+                  if (_currentLocation != null && widget.readOnly)
+                    Marker(
+                      point: _currentLocation!,
+                      width: 40.0,
+                      height: 40.0,
+                      child: Transform.rotate(
+                        angle: bearing * pi / 180,
+                        child: const Icon(
+                          Icons.swipe_up_alt,
+                          size: 50.0,
+                          color: Colors.blue,
+                        ),
+                      ),
                     ),
-                  ),
-                  rotate: false),
-            if (_markerPosition != null)
-              Marker(
-                  point: _markerPosition!,
-                  width: 80.0,
-                  height: 80.0,
-                  child: const Icon(
-                    Icons.location_on,
-                    size: 40.0,
-                    color: Colors.red,
-                  ),
-                  rotate: true),
-          ],
+                  if (_markerPosition != null)
+                    Marker(
+                      point: _markerPosition!,
+                      width: 80.0,
+                      height: 80.0,
+                      child: const Icon(
+                        Icons.push_pin,
+                        size: 40.0,
+                        color: Colors.red,
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
         ),
+        if (_markerPosition != null && _currentLocation != null)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text('Distance to Pin: ${distance.toStringAsFixed(2)} meters'),
+              ],
+            ),
+          ),
       ],
     );
   }
